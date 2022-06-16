246520edcd68fcc8b1ea17827bf57ea47e66c464
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-06-16 15:26:12 +0700

    Update to 0.0.0.1a

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
+import { useEffect } from 'react';
+import GlobalStore from '../GlobalStore/GlobalStore';
 import { Layout, Tabs, ConfigProvider, Spin, Button } from 'antd';
 import ru_RU from 'antd/lib/locale/ru_RU';
 import { observer, Provider } from 'mobx-react';
@@ -7,16 +9,34 @@ const { Header, Sider, Content } = Layout;
 const { TabPane } = Tabs;
 import '../CSS/App.css';
 import 'antd/dist/antd.css';
+import { ApiFetch } from '../Helpers/Helpers';
 
 const App = observer(() => {
-  const RequestApplicationMenu = () => {};
+  const RequestApplicationMenu = () => {
+    ApiFetch(
+      'api/configuration/GetApplicationMenu',
+      'GET',
+      undefined,
+      (Response) => {
+        GlobalStore.SetNewApplicationMenu(Response.Data.Items);
+      }
+    );
+  };
+  useEffect(() => {
+    RequestApplicationMenu();
+  }, []);
   return (
-    <Provider>
+    <Provider GlobalStore={GlobalStore}>
       <ConfigProvider locale={ru_RU}>
         <Layout>
           <Header></Header>
           <Layout>
             <Sider theme="light"></Sider>
+            {GlobalStore.ApplicationMenu.map((MenuElement) => {
+              return (
+                <Button key={MenuElement.Id}>{MenuElement.Caption} </Button>
+              );
+            })}
             <Tabs
               destroyInactiveTabPane={true}
               size="small"
--- src/GlobalStore/GlobalStore.js
+++ src/GlobalStore/GlobalStore.js
@@ -1,9 +1,17 @@
+const { makeAutoObservable } = require('mobx');
+
 class GlobalStore {
   OpenTabs = [];
   CurrentTabKey = null;
-  TopMenu = [];
+  ApplicationMenu = [];
 
   constructor() {
     makeAutoObservable(this);
   }
+  SetNewApplicationMenu(NewApplicationMenu) {
+    this.ApplicationMenu = NewApplicationMenu;
+  }
 }
+
+const Store = new GlobalStore();
+export default Store;
