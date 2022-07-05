ab0b288a6836c608e3cc5b044c88e8e4632dd889
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-07-05 15:48:27 +0700

    Update to 0.0.0.4

--- src/Components/CRUDComponent.jsx
+++ src/Components/CRUDComponent.jsx
@@ -1,20 +1,24 @@
-import { Button, Input, Table } from 'antd';
+import { Button, Input, message, Modal, Table } from 'antd';
 import { inject, observer } from 'mobx-react';
 import { nanoid } from 'nanoid';
 import React, { useEffect, useState } from 'react';
-import { ApiFetch } from '../Helpers/Helpers';
+import { ApiFetch, TableSorter } from '../Helpers/Helpers';
 import {
   RowButtonsWrapperStyle,
   RowInputStyle,
   RowStyle,
   RowTablePointerStyle,
 } from '../Styles/TableStyles';
+import { SearchOutlined } from '@ant-design/icons';
 import TableButtonBar from './TableButtonBar';
 
 const CRUDComponent = inject('GlobalStore')(
   observer((props) => {
     const [ObjectTable, SetNewObjectTable] = useState([]);
     const [SelectedKey, SetNewSelectedKey] = useState(null);
+    const [SearchString, SetNewSearchString] = useState(null);
+    const InputRef = React.createRef();
+    const SearchRef = React.createRef();
     const RequestData = () => {
       ApiFetch(
         `api/${props.GlobalStore.GetCurrentTab.GetCurrentMenuElementKey}`,
@@ -31,17 +35,128 @@ const CRUDComponent = inject('GlobalStore')(
         }
       );
     };
-    const EditObject = (Index) => {
-      let NewObjectTable = [...ObjectTable];
-      NewObjectTable[Index].Edited = true;
-      SetNewObjectTable(NewObjectTable);
+    const DeleteObject = () => {
+      let DeleteIndex = ObjectTable.findIndex((Object) => {
+        return Object.Key == SelectedKey;
+      });
+
+      if ('Id' in ObjectTable[DeleteIndex]) {
+        ApiFetch(
+          `api/${props.GlobalStore.GetCurrentTab.GetCurrentMenuElementKey}/${ObjectTable[DeleteIndex].Id}`,
+          'DELETE',
+          undefined,
+          (Response) => {
+            RequestData();
+          }
+        );
+      } else {
+        let NewObjectTable = [...ObjectTable];
+        NewObjectTable.splice(DeleteIndex, 1);
+        SetNewObjectTable(NewObjectTable);
+      }
+      SetNewSelectedKey(null);
+    };
+    const ShowDeleteModal = () => {
+      if (SelectedKey != null) {
+        Modal.confirm({
+          title: 'Подтвердите действие',
+          content: 'Вы действительно хотите удалить объект?',
+          okButtonProps: { type: 'primary', danger: true, size: 'small' },
+          cancelButtonProps: { size: 'small' },
+          okText: 'Удалить',
+          cancelText: 'Отмена',
+          onOk: () => {
+            DeleteObject();
+          },
+        });
+      }
+    };
+    const SaveObject = (Key) => {
+      if (
+        InputRef.current.input.value.length == 0 ||
+        ObjectTable.some((Object) => {
+          return Object.Caption == InputRef.current.input.value;
+        })
+      ) {
+        message.warning('Выберете другое наименование');
+      } else {
+        let SavedObject = ObjectTable.find((Object) => {
+          return Object.Key == Key;
+        });
+        SavedObject.Caption = InputRef.current.input.value;
+        ApiFetch(
+          `api/${props.GlobalStore.GetCurrentTab.GetCurrentMenuElementKey}${
+            'Id' in SavedObject ? `/${SavedObject.Id}` : ''
+          }`,
+          `${'Id' in SavedObject ? 'PATCH' : 'POST'}`,
+          SavedObject,
+          (Response) => {
+            RequestData();
+          }
+        );
+      }
+    };
+    const AddObject = () => {
+      if (
+        ObjectTable.some((Object) => {
+          return Object.Edited;
+        })
+      ) {
+        message.warning('Завершите редактирование объекта');
+      } else {
+        let NewObjectsTable = [...ObjectTable];
+        NewObjectsTable.unshift({ Caption: '', Edited: true, Key: nanoid() });
+        SetNewObjectTable(NewObjectsTable);
+      }
+    };
+    const CancelEdit = (Key) => {
+      let NewObjectsTable = [...ObjectTable];
+      let ObjectIndex = NewObjectsTable.findIndex((Object) => {
+        return Object.Key == Key;
+      });
+      if ('Id' in NewObjectsTable[ObjectIndex]) {
+        NewObjectsTable[ObjectIndex].Edited = false;
+      } else {
+        NewObjectsTable.splice(ObjectIndex, 1);
+        SetNewSelectedKey(null);
+      }
+      SetNewObjectTable(NewObjectsTable);
+    };
+    const EditObject = (Key) => {
+      if (
+        ObjectTable.some((Object) => {
+          return Object.Edited;
+        })
+      ) {
+        message.warning('Завершите редактирование объекта');
+      } else {
+        let NewObjectTable = [...ObjectTable];
+        let ObjectIndex = NewObjectTable.findIndex((Object) => {
+          return Object.Key == Key;
+        });
+        NewObjectTable[ObjectIndex].Edited = true;
+        SetNewObjectTable(NewObjectTable);
+      }
+    };
+    const ClearSearch = (Event) => {
+      if (Event.key == 'Escape') {
+        SetNewSearchString(null);
+      }
     };
     useEffect(() => {
+      document.addEventListener('keydown', ClearSearch, false);
       RequestData();
     }, []);
     return (
       <>
-        <TableButtonBar />
+        <TableButtonBar
+          OnAdd={() => {
+            AddObject();
+          }}
+          OnDelete={() => {
+            ShowDeleteModal();
+          }}
+        />
         <Table
           scroll={{ y: 700 }}
           rowSelection={{
@@ -58,7 +173,7 @@ const CRUDComponent = inject('GlobalStore')(
                 SetNewSelectedKey(Record.Key);
               },
               onDoubleClick: () => {
-                EditObject(Index);
+                EditObject(Record.Key);
               },
             };
           }}
@@ -68,20 +183,57 @@ const CRUDComponent = inject('GlobalStore')(
           size="small"
           columns={[
             {
+              defaultSortOrder: 'ascend',
+              sorter: TableSorter('Caption'),
+              onFilter: (Value, Record) => {
+                if (SearchString != null) {
+                  return Record[Value].toString()
+                    .toLowerCase()
+                    .includes(SearchString);
+                } else {
+                  return true;
+                }
+              },
+              filteredValue: ['Caption'],
               title: 'Наименование',
               dataIndex: 'Caption',
               key: 'Caption',
+              filterIcon: <SearchOutlined />,
+              filterDropdown: () => (
+                <Input
+                  placeholder="Поиск"
+                  size="small"
+                  ref={SearchRef}
+                  onPressEnter={(Event) => {
+                    SetNewSearchString(SearchRef.current.input.value);
+                  }}
+                />
+              ),
               render: (Value, Record, Index) =>
                 Record.Edited ? (
                   <RowStyle>
                     <RowInputStyle>
-                      <Input size="small" defaultValue={Value} />
+                      <Input size="small" defaultValue={Value} ref={InputRef} />
                     </RowInputStyle>
                     <RowButtonsWrapperStyle>
-                      <Button size="small" type="primary">
+                      <Button
+                        size="small"
+                        type="primary"
+                        onClick={() => {
+                          SaveObject(Record.Key);
+                        }}
+                      >
                         Сохранить
                       </Button>
-                      <Button size="small">Отмена</Button>
+                      <Button
+                        size="small"
+                        onClick={(Event) => {
+                          Event.stopPropagation();
+                          CancelEdit(Record.Key);
+                        }}
+                      >
+                        Отмена
+                      </Button>
                     </RowButtonsWrapperStyle>
                   </RowStyle>
                 ) : (
--- src/Helpers/Helpers.js
+++ src/Helpers/Helpers.js
@@ -1,3 +1,5 @@
+import Moment from 'moment';
+
 export const ApiFetch = (Adress, Method, Body, Callback) => {
   return new Promise((resolve, reject) => {
     fetch(Adress, { body: JSON.stringify(Body), method: Method })
@@ -18,3 +20,55 @@ export const ApiFetch = (Adress, Method, Body, Callback) => {
       });
   });
 };
+
+export function TableSorter(Feeld) {
+  return (a, b) => {
+    if (
+      Moment(a[Feeld], 'DD.MM.YYYY hh:mm:ss').unix() >
+      Moment(b[Feeld], 'DD.MM.YYYY hh:mm:ss').unix()
+    ) {
+      return 1;
+    }
+    if (
+      Moment(a[Feeld], 'DD.MM.YYYY hh:mm:ss').unix() <
+      Moment(b[Feeld], 'DD.MM.YYYY hh:mm:ss').unix()
+    ) {
+      return -1;
+    }
+    if (
+      Moment(a[Feeld], 'DD.MM.YYYY hh:mm:ss').unix() ==
+      Moment(b[Feeld], 'DD.MM.YYYY hh:mm:ss').unix()
+    ) {
+      return 0;
+    }
+
+    if (a[Feeld] > b[Feeld]) {
+      return 1;
+    }
+    if (a[Feeld] < b[Feeld]) {
+      return -1;
+    }
+    if (a[Feeld] == b[Feeld]) {
+      return 0;
+    }
+    if (a[Feeld].length == 0) {
+      return 1;
+    }
+    if (b[Feeld].length == 0) {
+      return -1;
+    }
+    if (a[Feeld].length == 0 && b[Feeld].length == 0) {
+      return 0;
+    }
+
+    if (a[Feeld].toLowerCase() < b[Feeld].toLowerCase()) {
+      return -1;
+    }
+    if (a[Feeld].toLowerCase() > b[Feeld].toLowerCase()) {
+      return 1;
+    }
+    if (a[Feeld].toLowerCase() == b[Feeld].toLowerCase()) {
+      return 0;
+    }
+  };
+}
