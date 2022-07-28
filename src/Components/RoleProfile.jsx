cbdb99a01102290c35716eddfc7460920b6da1e3
Mr.Amperage <Mr.Amperage@protonmail.com>
2022-07-28 10:51:53 +0700

    Update to 0.0.0.6e

--- src/Components/RoleProfile.jsx
+++ src/Components/RoleProfile.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { inject, observer } from 'mobx-react';
 import {
   ProfileRowElement,
   ProfileWrapper,
@@ -8,47 +7,52 @@ import {
 
 import { Collapse, Input, Tree } from 'antd';
 const { Panel } = Collapse;
-const UserProfile = inject('GlobalStore')(
-  observer((props) => {
-    return (
-      <>
-        <ProfileWrapper GridRowsTemplate="1fr">
-          <RowProfileWrapper>
-            <ProfileRowElement>Роль:</ProfileRowElement>
-            <ProfileRowElement>
-              <Input
-                onChange={(Event) => {
-                  props.ProfileHandler('Rolename', Event.target.value);
-                }}
-                size="small"
-                width="190px"
-                value={props.Profile.Rolename}
-              />
-            </ProfileRowElement>
-          </RowProfileWrapper>
-        </ProfileWrapper>
-        <Collapse>
-          <Panel header="Меню пользователя">
-            <Tree
-              fieldNames={{ title: 'Caption', key: 'Id', children: 'LeftMenu' }}
-              onCheck={(Checked) => {
-                props.ProfileHandler('MenusAccess', Checked);
+export default function RoleProfile(props) {
+  return (
+    <>
+      <ProfileWrapper GridRowsTemplate="1fr">
+        <RowProfileWrapper>
+          <ProfileRowElement>Роль:</ProfileRowElement>
+          <ProfileRowElement>
+            <Input
+              onChange={(Event) => {
+                props.ProfileHandler('Rolename', Event.target.value);
               }}
-              checkedKeys={props.Profile.MenusAccess}
-              treeData={props.UserMenu}
-              checkable={true}
-              selectable={false}
+              size="small"
+              width="190px"
+              value={props.Profile.Rolename}
             />
-          </Panel>
-        </Collapse>
-        <Collapse>
-          <Panel header="Организации и участки">
-            <Tree />
-          </Panel>
-        </Collapse>
-      </>
-    );
-  })
-);
-
-export default UserProfile;
+          </ProfileRowElement>
+        </RowProfileWrapper>
+      </ProfileWrapper>
+      <Collapse>
+        <Panel header="Меню пользователя">
+          <Tree
+            fieldNames={{ title: 'Caption', key: 'Id', children: 'LeftMenu' }}
+            onCheck={(Checked) => {
+              props.ProfileHandler('MenusAccess', Checked);
+            }}
+            checkedKeys={props.Profile.MenusAccess}
+            treeData={props.UserMenu}
+            checkable={true}
+            selectable={false}
+          />
+        </Panel>
+      </Collapse>
+      <Collapse>
+        <Panel header="Группы">
+          <Tree
+            checkable={true}
+            selectable={false}
+            fieldNames={{ title: 'Caption', key: 'Id' }}
+            treeData={props.Groups}
+            onCheck={(Checked) => {
+              props.ProfileHandler('OrganizationsAccess', Checked);
+            }}
+            checkedKeys={props.Profile.OrganizationsAccess}
+          />
+        </Panel>
+      </Collapse>
+    </>
+  );
+}
--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -1,5 +1,4 @@
 import { Button, Checkbox, DatePicker, Input, message, Select } from 'antd';
-import { inject, observer } from 'mobx-react';
 import Bcrypt from 'bcryptjs';
 import React, { useState } from 'react';
 
@@ -11,144 +10,137 @@ import {
 } from '../Styles/ProfileStyles';
 import { RowStyle } from '../Styles/TableStyles';
 import Moment from 'moment';
-const UserProfile = inject('GlobalStore')(
-  observer((props) => {
-    const [ShowPasswordInput, SetNewShowPasswordInput] = useState(false);
-    const SavePassword = (Password) => {
-      if (Password.length > 0) {
-        return Bcrypt.hash(Password, 12);
-      } else {
-        return Promise.reject('Пароль не может быть пустым');
-      }
-    };
-    return (
-      <ProfileWrapper GridRowsTemplate="1fr 1fr 1fr 1fr 1fr 1fr">
-        <RowProfileWrapper>
-          <ProfileRowElement>Имя пользователя:</ProfileRowElement>
-          <ProfileRowElement>
-            <Input
-              onChange={(Event) => {
-                props.ProfileHandler('Username', Event.target.value);
-              }}
-              size="small"
-              width="190px"
-              value={props.Profile.Username}
-            />
-          </ProfileRowElement>
-        </RowProfileWrapper>
-        <RowProfileWrapper>
-          <ProfileRowElement>Пароль:</ProfileRowElement>
-          <ProfileRowElement>
-            {ShowPasswordInput ? (
-              <RowStyle width="197px" justifyContent="space-between">
-                <Input
-                  type="password"
-                  size="small"
-                  value={props.Profile.NotHashedPassword}
-                  onChange={(Event) => {
-                    props.ProfileHandler(
-                      'NotHashedPassword',
-                      Event.target.value
-                    );
-                  }}
-                />
-                <RowStyle width="140px" justifyContent="space-evenly">
-                  <Button
-                    onClick={() => {
-                      SavePassword(props.Profile.NotHashedPassword)
-                        .then((Password) => {
-                          props.ProfileHandler('Password', Password);
-                          SetNewShowPasswordInput(false);
-                        })
-                        .catch((Error) => {
-                          message.warning(Error);
-                        });
-                    }}
-                    icon={<CheckOutlined />}
-                    size="small"
-                    type="primary
-                "
-                  />
-                  <Button
-                    onClick={() => {
-                      SetNewShowPasswordInput(false);
-                    }}
-                    size="small"
-                    icon={<CloseOutlined />}
-                    type="primary"
-                    danger={true}
-                  />
-                </RowStyle>
-              </RowStyle>
-            ) : (
-              <Button
+export default function UserProfile(props) {
+  const [ShowPasswordInput, SetNewShowPasswordInput] = useState(false);
+  const SavePassword = (Password) => {
+    if (Password.length > 0) {
+      return Bcrypt.hash(Password, 12);
+    } else {
+      return Promise.reject('Пароль не может быть пустым');
+    }
+  };
+  return (
+    <ProfileWrapper GridRowsTemplate="1fr 1fr 1fr 1fr 1fr 1fr">
+      <RowProfileWrapper>
+        <ProfileRowElement>Имя пользователя:</ProfileRowElement>
+        <ProfileRowElement>
+          <Input
+            onChange={(Event) => {
+              props.ProfileHandler('Username', Event.target.value);
+            }}
+            size="small"
+            width="190px"
+            value={props.Profile.Username}
+          />
+        </ProfileRowElement>
+      </RowProfileWrapper>
+      <RowProfileWrapper>
+        <ProfileRowElement>Пароль:</ProfileRowElement>
+        <ProfileRowElement>
+          {ShowPasswordInput ? (
+            <RowStyle width="197px" justifyContent="space-between">
+              <Input
+                type="password"
                 size="small"
-                type="primary"
-                onClick={() => {
-                  SetNewShowPasswordInput(true);
+                value={props.Profile.NotHashedPassword}
+                onChange={(Event) => {
+                  props.ProfileHandler('NotHashedPassword', Event.target.value);
                 }}
-              >
-                Установить новый пароль
-              </Button>
-            )}
-          </ProfileRowElement>
-        </RowProfileWrapper>
-        <RowProfileWrapper>
-          <ProfileRowElement>Выбор роли:</ProfileRowElement>
-          <ProfileRowElement>
-            <Select
-              dropdownMatchSelectWidth={130}
-              onChange={(Value) => {
-                props.ProfileHandler('RoleId', Value);
-              }}
+              />
+              <RowStyle width="140px" justifyContent="space-evenly">
+                <Button
+                  onClick={() => {
+                    SavePassword(props.Profile.NotHashedPassword)
+                      .then((Password) => {
+                        props.ProfileHandler('Password', Password);
+                        SetNewShowPasswordInput(false);
+                      })
+                      .catch((Error) => {
+                        message.warning(Error);
+                      });
+                  }}
+                  icon={<CheckOutlined />}
+                  size="small"
+                  type="primary
+              "
+                />
+                <Button
+                  onClick={() => {
+                    SetNewShowPasswordInput(false);
+                  }}
+                  size="small"
+                  icon={<CloseOutlined />}
+                  type="primary"
+                  danger={true}
+                />
+              </RowStyle>
+            </RowStyle>
+          ) : (
+            <Button
               size="small"
-              options={props.Roles}
-              value={props.Profile.RoleId}
-            />
-          </ProfileRowElement>
-        </RowProfileWrapper>
-        <RowProfileWrapper>
-          <ProfileRowElement>Доступ включен:</ProfileRowElement>
-          <ProfileRowElement>
-            <Checkbox
-              checked={props.Profile.Enabled}
-              onChange={(Event) => {
-                props.ProfileHandler('Enabled', Event.target.checked);
+              type="primary"
+              onClick={() => {
+                SetNewShowPasswordInput(true);
               }}
-            />
-          </ProfileRowElement>
-        </RowProfileWrapper>
-        <RowProfileWrapper>
-          <ProfileRowElement>Доступ с:</ProfileRowElement>
-          <ProfileRowElement>
-            <DatePicker
-              onOk={(MomentObject) => {
-                props.ProfileHandler('StartDate', MomentObject.format());
-              }}
-              format="DD.MM.YYY HH:mm:ss"
-              size="small"
-              showTime={true}
-              value={Moment(props.Profile.StartDate)}
-            />
-          </ProfileRowElement>
-        </RowProfileWrapper>
-        <RowProfileWrapper>
-          <ProfileRowElement>Доступ по:</ProfileRowElement>
-          <ProfileRowElement>
-            <DatePicker
-              onOk={(MomentObject) => {
-                props.ProfileHandler('EndDate', MomentObject.format());
-              }}
-              format="DD.MM.YYY HH:mm:ss"
-              size="small"
-              showTime={true}
-              value={Moment(props.Profile.EndDate)}
-            />
-          </ProfileRowElement>
-        </RowProfileWrapper>
-      </ProfileWrapper>
-    );
-  })
-);
-
-export default UserProfile;
+            >
+              Установить новый пароль
+            </Button>
+          )}
+        </ProfileRowElement>
+      </RowProfileWrapper>
+      <RowProfileWrapper>
+        <ProfileRowElement>Выбор роли:</ProfileRowElement>
+        <ProfileRowElement>
+          <Select
+            dropdownMatchSelectWidth={130}
+            onChange={(Value) => {
+              props.ProfileHandler('RoleId', Value);
+            }}
+            size="small"
+            options={props.Roles}
+            value={props.Profile.RoleId}
+          />
+        </ProfileRowElement>
+      </RowProfileWrapper>
+      <RowProfileWrapper>
+        <ProfileRowElement>Доступ включен:</ProfileRowElement>
+        <ProfileRowElement>
+          <Checkbox
+            checked={props.Profile.Enabled}
+            onChange={(Event) => {
+              props.ProfileHandler('Enabled', Event.target.checked);
+            }}
+          />
+        </ProfileRowElement>
+      </RowProfileWrapper>
+      <RowProfileWrapper>
+        <ProfileRowElement>Доступ с:</ProfileRowElement>
+        <ProfileRowElement>
+          <DatePicker
+            onOk={(MomentObject) => {
+              props.ProfileHandler('StartDate', MomentObject.format());
+            }}
+            format="DD.MM.YYY HH:mm:ss"
+            size="small"
+            showTime={true}
+            value={Moment(props.Profile.StartDate)}
+          />
+        </ProfileRowElement>
+      </RowProfileWrapper>
+      <RowProfileWrapper>
+        <ProfileRowElement>Доступ по:</ProfileRowElement>
+        <ProfileRowElement>
+          <DatePicker
+            onOk={(MomentObject) => {
+              props.ProfileHandler('EndDate', MomentObject.format());
+            }}
+            format="DD.MM.YYY HH:mm:ss"
+            size="small"
+            showTime={true}
+            value={Moment(props.Profile.EndDate)}
+          />
+        </ProfileRowElement>
+      </RowProfileWrapper>
+    </ProfileWrapper>
+  );
+}
